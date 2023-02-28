cef6ee81b9f07bade51a7e3043ec03686fb3ab63
Natanael Forte <natanaelforte@gmail.com>
2023-02-28 14:59:39 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+void main() => runApp( MeuApp() );
+
+class MeuApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('IMC Calculadora'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: const Center(
+          child: Text('Olá Mundo!'),
+        ),
+      ),
+    );
+  }
+
+}
