384654c77d9403a7e7c50bb03f011f70ca3cb074
Natanael Forte <natanaelforte@gmail.com>
2023-03-02 16:45:04 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,32 @@ class MeuApp extends StatelessWidget{
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          children: const [
             Icon( Icons.person_outlined, 
             size: 170.0, 
-            color: Color.fromARGB(255, 120, 12, 124),)
+            color: Color.fromARGB(255, 120, 12, 124),),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Peso',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 120, 12, 124),
+                  fontSize: 20.0),
+                border: OutlineInputBorder(),  
+              ),
+              style: TextStyle(color: Colors.black, fontSize: 25.0),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Altura',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 120, 12, 124),
+                  fontSize: 20.0),
+                border: OutlineInputBorder(),  
+              ),
+              style: TextStyle(color: Colors.black, fontSize: 25.0),
+              keyboardType: TextInputType.number,
+            )
           ],
         ),
       ),
