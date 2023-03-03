9b06470b0d94dec14e87021ce6ca1f5a8a448ac4
Natanael Forte <natanaelforte@gmail.com>
2023-03-03 14:56:19 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,31 @@ import "package:flutter/material.dart";
 void main() => runApp( MeuApp() );
 
 class MeuApp extends StatelessWidget{
+  
+
+
+  Widget buildTextField(String label) {
+    return Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children:   [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: label,
+                      labelStyle: const TextStyle(
+                        color: Color.fromARGB(255, 120, 12, 124),
+                        fontSize: 20.0),
+                      border: OutlineInputBorder(),  
+                    ),
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
+                    keyboardType: TextInputType.number,
+                  ),
+                  
+                ],
+              ),
+              );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,36 +44,27 @@ class MeuApp extends StatelessWidget{
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Icon( Icons.person_outlined, 
-            size: 170.0, 
-            color: Color.fromARGB(255, 120, 12, 124),),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Peso',
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 120, 12, 124),
-                  fontSize: 20.0),
-                border: OutlineInputBorder(),  
-              ),
-              style: TextStyle(color: Colors.black, fontSize: 25.0),
-              keyboardType: TextInputType.number,
+          children:   [
+            const Icon( Icons.person_outlined, 
+             size: 170.0, 
+             color: Color.fromARGB(255, 120, 12, 124),),
+            buildTextField("Peso"),
+            buildTextField("Altura"),
+            floatingActionButton: FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: Container(
+              Padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Verificar'),
             ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Altura',
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 120, 12, 124),
-                  fontSize: 20.0),
-                border: OutlineInputBorder(),  
-              ),
-              style: TextStyle(color: Colors.black, fontSize: 25.0),
-              keyboardType: TextInputType.number,
             )
           ],
+          
         ),
       ),
     );
   }
 
 }
+
+
